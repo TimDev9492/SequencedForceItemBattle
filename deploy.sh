85f902e7990c48e3b3887386365b85e962de5a73
Tim Kluge <tim@timdev.de>
2026-04-16 15:05:53 +0200

    fix: update gradle shadow plugin version and fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 
 key_file="/home/tim/.ssh/id_ed25519_netcup"
 host="chopin@netcup"
-destination="/home/chopin/minecraft/servers/plugin-dev/plugins/"
-screen_session_name="minecraft-server-plugin-dev"
+docker_dir="/opt/docker/minecraft"
+destination="$docker_dir/data/servers/challenges/plugins/"
+screen_session_name="minecraft-server-challenges"
 
 # $1 - the path to the jar file
 if [ -z "$1" ]; then echo "No jar file provided!"; exit 1; fi
@@ -18,6 +19,6 @@ EOF
 
 # reload the server
 echo "Reloading server..."
-ssh -i "$key_file" "$host" "screen -S \"$screen_session_name\" -p 0 -X stuff \"plugman reload SequencedForceItemBattle\"\`echo -ne '\015'\`"
+ssh -i "$key_file" "$host" "cd \"$docker_dir\" && docker compose exec server-mgr screen -S \"$screen_session_name\" -p 0 -X stuff \"plugman reload SequencedForceItemBattle\"\`echo -ne '\015'\`"
 if [ $? -eq 1 ]; then echo "Failed to reload the server."; exit 0; fi
 echo "Success!"; exit 0
